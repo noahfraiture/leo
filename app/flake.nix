0f670f3303a4854ac6af83c5b7eb6ce41e81d61b
Noah Fraiture <pro@noahcode.dev>
2026-07-19 12:38:19 +0200

    chore: flake.nix

---
+++ app/flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, ... }@inputs:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [
+          # Overlay which add rust-bin to pkgs
+          inputs.rust-overlay.overlays.default
+          (final: prev: {
+            rustToolchain = prev.rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            };
+          })
+        ];
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+
+        packages = with pkgs; [
+          just
+          cargo-watch
+          rustToolchain
+        ];
+
+        DIRENV = "Leo";
+      };
+    };
+}
